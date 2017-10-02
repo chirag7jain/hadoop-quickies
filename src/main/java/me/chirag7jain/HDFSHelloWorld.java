7e12c50a971a3852f666358a80792329b048f038
cj <chirag7jain@gmail.com>
2017-10-01 17:36:41 -0700

    Hadoop HelloWorld example from YAHOO!

---
+++ src/main/java/me/chirag7jain/HDFSHelloWorld.java
@@ -0,0 +1,43 @@
+package me.chirag7jain;
+
+// SRC - https://developer.yahoo.com/hadoop/tutorial/module2.html
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import java.io.IOException;
+
+public class HDFSHelloWorld {
+    public static final String theFilename = "hello.txt";
+    public static final String message = "Hello, world!\n";
+
+    public static void main (String [] args) throws IOException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+
+        Path filenamePath = new Path(theFilename);
+
+        try {
+            if (fs.exists(filenamePath)) {
+                // remove the file first
+                fs.delete(filenamePath);
+            }
+
+            FSDataOutputStream out = fs.create(filenamePath);
+            out.writeUTF(message);
+            out.close();
+
+            FSDataInputStream in = fs.open(filenamePath);
+            String messageIn = in.readUTF();
+            System.out.print(messageIn);
+            in.close();
+        }
+        catch (IOException ioe) {
+            System.err.println("IOException during operation: " + ioe.toString());
+            System.exit(1);
+        }
+    }
+}
+
